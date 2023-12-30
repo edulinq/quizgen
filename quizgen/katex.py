2a9fd873d7094557f12687a0a024e3dfce6a2c7e
Eriq Augustine <eriq.augustine@gmail.com>
2023-12-30 11:42:53 +0700

    Changed the path KaTeX is searched for. Hoping to fix an issue in WSL, but not guarenteed yet.

--- quizgen/katex.py
+++ quizgen/katex.py
@@ -1,21 +1,17 @@
-import os
 import subprocess
 import sys
 
-THIS_DIR = os.path.abspath(os.path.dirname(os.path.realpath(__file__)))
-ROOT_DIR = os.path.join(THIS_DIR, '..')
-
 ENCODING = 'utf-8'
 
-def _has_command(command, cwd = ROOT_DIR):
+def _has_command(command, cwd = '.'):
     result = subprocess.run(["which", command], cwd = cwd, capture_output = True)
     return (result.returncode == 0)
 
-def _has_package(package, cwd = ROOT_DIR):
+def _has_package(package, cwd = '.'):
     result = subprocess.run(["npm", "list", package], cwd = cwd, capture_output = True)
     return (result.returncode == 0)
 
-def is_available(cwd = ROOT_DIR):
+def is_available(cwd = '.'):
     if (not _has_command('npx', cwd = cwd)):
         print("WARN: Could not find `npx` (usually installed with `npm`), cannot use katex equations.", file = sys.stderr)
         return False
@@ -26,7 +22,7 @@ def is_available(cwd = ROOT_DIR):
 
     return True
 
-def to_html(text, cwd = ROOT_DIR):
+def to_html(text, cwd = '.'):
     result = subprocess.run(["npx", "katex", "--format", "mathml"], cwd = cwd,
         input = text, encoding = ENCODING,
         capture_output = True, check = True)
