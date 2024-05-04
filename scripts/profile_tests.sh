65bc1eb1a6c53001fa2dcb85b6320418bf73450a
Eriq Augustine <eriq.augustine@gmail.com>
2024-05-04 20:55:44 +0200

    Added a script to help profile tests.

---
+++ scripts/profile_tests.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+
+# Profile run_tests.py.
+# Any arguments passed to this script will be forwarded to run_tests.py.
+
+readonly THIS_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+readonly ROOT_DIR="${THIS_DIR}/.."
+
+readonly RUN_TESTS_RELPATH="run_tests.py"
+
+readonly TEMP_STATS_PATH="/tmp/quizgen_profile_stats.cprofile"
+
+readonly ROW_COUNT=50
+
+function main() {
+    set -e
+    trap exit SIGINT
+
+    cd "${ROOT_DIR}"
+
+    echo "Profiling ..."
+    python -m cProfile -o "${TEMP_STATS_PATH}" "${RUN_TESTS_RELPATH}" "$@" > "${TEMP_STATS_PATH}.out" 2> "${TEMP_STATS_PATH}.err"
+    echo "Profiling Complete"
+    echo ""
+
+    echo "--- BEGIN: All Functions, Sorted by Cummulitive Time, Top ${ROW_COUNT} ---"
+    python -c "import pstats ; stats = pstats.Stats('${TEMP_STATS_PATH}') ; stats.sort_stats('cumtime').print_stats(${ROW_COUNT})"
+    echo "--- END: All Functions, Sorted by Cummulitive Time, Top ${ROW_COUNT} ---"
+
+    echo "--- BEGIN: All Functions, Sorted by Total Time, Top ${ROW_COUNT} ---"
+    python -c "import pstats ; stats = pstats.Stats('${TEMP_STATS_PATH}') ; stats.sort_stats('tottime').print_stats(${ROW_COUNT})"
+    echo "--- END: All Functions, Sorted by Total Time, Top ${ROW_COUNT} ---"
+
+    echo "--- BEGIN: Quizgen Functions, Sorted by Cummulitive Time, Top ${ROW_COUNT} ---"
+    python -c "import pstats ; stats = pstats.Stats('${TEMP_STATS_PATH}') ; stats.sort_stats('cumtime').print_stats('quizgen', ${ROW_COUNT})"
+    echo "--- END: Quizgen Functions, Sorted by Cummulitive Time, Top ${ROW_COUNT} ---"
+
+    echo "--- BEGIN: Quizgen Functions, Sorted by Total Time, Top ${ROW_COUNT} ---"
+    python -c "import pstats ; stats = pstats.Stats('${TEMP_STATS_PATH}') ; stats.sort_stats('tottime').print_stats('quizgen', ${ROW_COUNT})"
+    echo "--- END: Quizgen Functions, Sorted by Total Time, Top ${ROW_COUNT} ---"
+}
+
+[[ "${BASH_SOURCE[0]}" == "${0}" ]] && main "$@"
