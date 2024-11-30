1e23d55d537c7426e3281d098bebbaa1d3c304df
Eriq Augustine <eriq.augustine@gmail.com>
2024-11-29 21:35:09 -0800

    Made custom mdformat renderers non-static to prevent issues in Pythno 3.9.

--- quizgen/parser/renderer/markdown.py
+++ quizgen/parser/renderer/markdown.py
@@ -4,37 +4,32 @@ import mdformat.renderer
 import quizgen.parser.common
 import quizgen.parser.math
 
+def math_inline(node, context):
+    qg_context = context.env.get(quizgen.parser.common.CONTEXT_ENV_KEY, {})
+    return quizgen.parser.math._render_md(node.content, True, qg_context)
+
+def math_block(node, context):
+    qg_context = context.env.get(quizgen.parser.common.CONTEXT_ENV_KEY, {})
+    return quizgen.parser.math._render_md(node.content, False, qg_context)
+
+def placeholder(node, context):
+    return "<placeholder>%s</placeholder>" % (node.content)
+
+def container_block(node, context):
+    # We can ignore blocks when outputting markdown (since it is non-standard).
+    # Just render the child node (there should only be one).
+    if ((node.children is None) or (len(node.children) == 0)):
+        return ''
+
+    parts = []
+    for child in node.children:
+        parts.append(child.render(context))
+
+    return "\n\n".join(parts)
+
 class QuizgenMDformatExtension(mdformat.plugins.ParserExtensionInterface):
     CHANGES_AST = False
     POSTPROCESSORS = {}
-
-    @staticmethod
-    def math_inline(node, context):
-        qg_context = context.env.get(quizgen.parser.common.CONTEXT_ENV_KEY, {})
-        return quizgen.parser.math._render_md(node.content, True, qg_context)
-
-    @staticmethod
-    def math_block(node, context):
-        qg_context = context.env.get(quizgen.parser.common.CONTEXT_ENV_KEY, {})
-        return quizgen.parser.math._render_md(node.content, False, qg_context)
-
-    @staticmethod
-    def placeholder(node, context):
-        return "<placeholder>%s</placeholder>" % (node.content)
-
-    @staticmethod
-    def container_block(node, context):
-        # We can ignore blocks when outputting markdown (since it is non-standard).
-        # Just render the child node (there should only be one).
-        if ((node.children is None) or (len(node.children) == 0)):
-            return ''
-
-        parts = []
-        for child in node.children:
-            parts.append(child.render(context))
-
-        return "\n\n".join(parts)
-
     RENDERERS = {
         'container_block': container_block,
         'math_block': math_block,
