bfee285824e20409fcf56e881e058c04866a14f5
Eriq Augustine <eriq.augustine@gmail.com>
2024-12-09 14:55:22 -0600

    Added the concept of a 'project' to the QuizGen. Added a simple CLI tool for reading projects.

---
+++ quizgen/cli/project/read.py
@@ -0,0 +1,55 @@
+"""
+Read (an optionally save) a QuizGen project.
+"""
+
+import argparse
+import sys
+
+import quizgen.log
+import quizgen.project
+
+def run(args):
+    project = quizgen.project.Project.from_path(args.path)
+
+    quizzes, questions = project.load_resources()
+
+    print("Found %d quizzes." % (len(quizzes)))
+    for (path, quiz) in quizzes:
+        print("    %s (%s)" % (path, quiz.title))
+
+    print("Found %d question." % (len(questions)))
+    for (path, question) in questions:
+        text = "    %s" % (path)
+        if (question.name != ''):
+            text += " (%s)" % (question.name)
+
+        print(text)
+
+    if (args.out_dir is not None):
+        project.save(args.out_dir)
+
+    return 0
+
+def _get_parser():
+    parser = argparse.ArgumentParser(description =
+        __doc__.strip())
+
+    parser.add_argument('path', metavar = 'PATH',
+        type = str,
+        help = 'The path to a project dir or config file.')
+
+    parser.add_argument('--outdir', dest = 'out_dir',
+        action = 'store', type = str, default = None,
+        help = 'Save the project to this directory.')
+
+    quizgen.log.set_cli_args(parser)
+
+    return parser
+
+def main():
+    args = _get_parser().parse_args()
+    quizgen.log.init_from_args(args)
+    return run(args)
+
+if (__name__ == '__main__'):
+    sys.exit(main())
---
+++ quizgen/project.py
@@ -0,0 +1,101 @@
+import glob
+import os
+
+import json5
+
+import quizgen.common
+import quizgen.constants
+import quizgen.question.base
+import quizgen.quiz
+import quizgen.util.dirent
+import quizgen.util.serial
+
+class Project(quizgen.util.serial.JSONSerializer):
+    """
+    A project object represents a directory that may contain quizzes and questions.
+    The directory (or any file structure) is never serialized.
+    """
+
+    def __init__(self,
+            name = '',
+            base_dir = '',
+            **kwargs):
+        super().__init__(**kwargs)
+
+        self.name = name
+        self._base_dir = base_dir
+
+        try:
+            self.validate(cls = Project, **kwargs)
+        except Exception as ex:
+            raise quizgen.common.QuizValidationError("Error while validating project '%s' ('%s')." % (self._base_dir, self.base_dir)) from ex
+
+    def _validate(self, **kwargs):
+        if ((self._base_dir is None) or (self._base_dir == "")):
+            raise quizgen.common.QuizValidationError("Base directory cannot be empty.")
+
+        if (not os.path.isdir(self._base_dir)):
+            raise quizgen.common.QuizValidationError("Base directory '%s' does not exist or is not a directory." % (self._base_dir))
+
+    def find_resources(self):
+        """
+        Find all the resources associated with this project.
+        Returns the path to these resources as (quizzes, questions).
+        """
+
+        quizzes = sorted(glob.glob(os.path.join(self._base_dir, '**', quizgen.constants.QUIZ_FILENAME), recursive = True))
+        questions = sorted(glob.glob(os.path.join(self._base_dir, '**', quizgen.constants.QUESTION_FILENAME), recursive = True))
+
+        return (quizzes, questions)
+
+    def load_resources(self, **kwargs):
+        """
+        Load (and validate) all the resources associated with this project.
+        Returns: ([(quiz path, quiz object), ...], [(question path, question object), ...])
+        """
+
+        quiz_paths, question_paths = self.find_resources()
+
+        quizzes = [(path, quizgen.quiz.Quiz.from_path(path, **kwargs)) for path in quiz_paths]
+        questions = [(path, quizgen.question.base.Question.from_path(path, **kwargs)) for path in question_paths]
+
+        return (quizzes, questions)
+
+    def save(self, out_dir = None):
+        """
+        Save this project to the specified dir,
+        or our own base dir if no other dir is specified.
+
+        Note that in the case we are saving to our own base dir,
+        this operation just (re)writes the project file.
+        """
+
+        if (out_dir is None):
+            out_dir = self._base_dir
+        out_dir = os.path.abspath(out_dir)
+
+        if (os.path.isfile(out_dir)):
+            raise ValueError("Output for project save cannot be a file, it must be a directory.")
+
+        # If we are pointing to the base_dir, then just save the project file and return.
+        if (os.path.isfile(out_dir) and os.path.samefile(out_dir, self._base_dir)):
+            self.to_path(os.path.join(self._base_dir, quizgen.constants.PROJECT_FILENAME))
+            return
+
+        quizgen.util.dirent.copy_dirent(self._base_dir, out_dir,
+                symlinks = True, dirs_exist_ok = True)
+        self.to_path(os.path.join(out_dir, quizgen.constants.PROJECT_FILENAME))
+
+    @classmethod
+    def from_path(cls, path, **kwargs):
+        # If we are looking at a dir, assume the project file is directly inside.
+        if (os.path.isdir(path)):
+            path = os.path.join(path, quizgen.constants.PROJECT_FILENAME)
+
+        path = os.path.abspath(path)
+
+        # If the project file does not exist, then continue with minimal info.
+        if (not os.path.exists(path)):
+            return Project(base_dir = os.path.dirname(path), **kwargs)
+
+        return super().from_path(path, **kwargs)
