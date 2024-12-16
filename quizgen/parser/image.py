ecdb466f8162e56288885e5ba4e5fcd508e4c0cb
Eriq Augustine <eriq.augustine@gmail.com>
2024-12-15 18:21:38 -0600

    Removed the image callback cache, as it was causing issues when compiling the same quiz multiple times in the same process. Hard to see without actually parsing the quiz output (like when compiling a PDF).

--- quizgen/parser/image.py
+++ quizgen/parser/image.py
@@ -3,23 +3,11 @@ import os
 
 import quizgen.parser.common
 
-# Cache the results of any image callbacks using (original src, base dir) as the key.
-_callback_cache = {}
-
 def handle_callback(callback, original_src, base_dir):
     if (callback is None):
         return original_src
 
-    key = (original_src, base_dir)
-
-    computed_src = _callback_cache.get(key, None)
-    if (computed_src is not None):
-        return computed_src
-
-    computed_src = callback(original_src, base_dir)
-    _callback_cache[key] = computed_src
-
-    return computed_src
+    return callback(original_src, base_dir)
 
 def encode_image(path):
     ext = os.path.splitext(path)[-1].lower()
