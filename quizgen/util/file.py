808c2f278599b5cccd7c9c0210d298bc30ce67b6
Eriq Augustine <eriq.augustine@gmail.com>
2024-12-17 11:08:44 -0600

    Fixed and old package rename.

--- quizgen/util/file.py
+++ quizgen/util/file.py
@@ -4,10 +4,10 @@ def to_base64(path):
     with open(path, 'rb') as file:
         data = file.read()
 
-    return qgg.util.encoding.to_base64(data)
+    return quizgen.util.encoding.to_base64(data)
 
 def from_base64(data, path):
-    data = qgg.util.encoding.from_base64(data)
+    data = quizgen.util.encoding.from_base64(data)
     with open(path, 'wb') as file:
         file.write(data)
 
