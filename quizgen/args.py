3588a30a15b8335005acee4621b80ca778098171
Eriq Augustine <eriq.augustine@gmail.com>
2024-12-17 11:00:56 -0600

    Added an option for setting the path to the pdflatex binary.

--- quizgen/args.py
+++ quizgen/args.py
@@ -13,6 +13,7 @@ import argparse
 
 # Import modules that have common args to ensure they are loaded.
 import quizgen.katex
+import quizgen.latex
 import quizgen.log
 
 # {module name: function(parser), ...}
@@ -59,3 +60,4 @@ def register(module_name, pre_callback = None, post_callback = None):
 # Register common args.
 register('log', quizgen.log.set_cli_args, quizgen.log.init_from_args)
 register('katex', quizgen.katex.set_cli_args, quizgen.katex.init_from_args)
+register('latex', quizgen.latex.set_cli_args, quizgen.latex.init_from_args)
--- quizgen/latex.py
+++ quizgen/latex.py
@@ -3,7 +3,16 @@ import os
 import shutil
 import subprocess
 
+_pdflatex_bin_path = None
+
+def set_pdflatex_bin_path(path):
+    global _pdflatex_bin_path
+    _pdflatex_bin_path = path
+
 def is_available():
+    if (_pdflatex_bin_path is not None):
+        return True
+
     if (shutil.which('pdflatex') is None):
         logging.warning("Could not find `pdxlatex`, cannot compile PDFs")
         return False
@@ -11,8 +20,27 @@ def is_available():
     return True
 
 def compile(path):
-    result = subprocess.run(['pdflatex', '-interaction=nonstopmode', os.path.basename(path)], cwd = os.path.dirname(path),
+    bin_path = 'pdflatex'
+    if (_pdflatex_bin_path is not None):
+        bin_path = _pdflatex_bin_path
+
+    result = subprocess.run([bin_path, '-interaction=nonstopmode', os.path.basename(path)], cwd = os.path.dirname(path),
         capture_output = True)
 
     if (result.returncode != 0):
         raise ValueError("pdflatex did not exit cleanly. Stdout: '%s', Stderr: '%s'" % (result.stdout, result.stderr))
+
+def set_cli_args(parser):
+    parser.add_argument('--pdflatex-bin-path', dest = 'pdflatex_bin_path',
+        action = 'store', type = str, default = None,
+        help = ('The path to the pdflatex binary to use.'
+                + ' If not specified, $PATH will be searched.'
+                + ' Used to compile PDFs.'))
+
+    return parser
+
+def init_from_args(args):
+    if (args.pdflatex_bin_path is not None):
+        set_pdflatex_bin_path(args.pdflatex_bin_path)
+
+    return args
